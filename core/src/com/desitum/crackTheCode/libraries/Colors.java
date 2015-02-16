e9a9c7315766cbd75e5bd831f6bde898b4b98b18
Zack Moss <zmyth97@gmail.com>
2015-02-15 19:48:26 -0700

    Added  Colors

---
+++ core/src/com/desitum/crackTheCode/libraries/Colors.java
@@ -0,0 +1,25 @@
+package com.desitum.crackTheCode.libraries;
+
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Zmyth97 on 2/15/2015.
+ */
+public class Colors {
+    public static final Color ACTIVE_CIRCLE = new Color(0.99f, 0.45f, 0.07f, 1);
+    public static final Color GAME_CIRCLE = new Color(0.13f, 0.17f, 0.96f, 1);
+
+
+    public static ArrayList<Color> Colors;
+
+
+    public static void load() {
+        Colors = new ArrayList<Color>();
+
+        Colors.add(ACTIVE_CIRCLE);
+        Colors.add(GAME_CIRCLE);
+    }
+
+}
