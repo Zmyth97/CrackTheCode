8240a55fa159223155ed5d2deaaeed39caf5abbe
Zmyth97 <zmyth97@gmail.com>
2015-02-17 12:56:18 -0700

    Drawing Added

--- core/src/com/desitum/crackTheCode/libraries/Animation.java
+++ core/src/com/desitum/crackTheCode/libraries/Animation.java
@@ -2,7 +2,6 @@ package com.desitum.crackTheCode.libraries;
 
 /**
  * Created by kody on 2/14/15.
- * can be used by kody and people in []
  */
 public interface Animation {
     public void update(float delta);
