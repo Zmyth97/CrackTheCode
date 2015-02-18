ff4bf58f29ebc903149a3f7c62b7537f101c41da
Zmyth97 <zmyth97@gmail.com>
2015-02-18 12:15:59 -0700

    Added Game Tiles

--- core/src/com/desitum/crackTheCode/objects/MenuButton.java
+++ core/src/com/desitum/crackTheCode/objects/MenuButton.java
@@ -25,7 +25,7 @@ public class MenuButton extends Sprite {
         this.setSize(SIZE_X, SIZE_Y);
         this.setColor(0, 0, 0.2f, 1);
 
-        this.colorChanger = new ColorEffects( new Color(0.98f, 0.85f, 0.4f, 1),this.getColor(), 3f);
+        this.colorChanger = new ColorEffects( new Color(0.98f, 0.85f, 0.4f, 1),this.getColor(), 0.5f);
     }
 
     public void update(float delta) {
--- core/src/com/desitum/crackTheCode/world/GameWorld.java
+++ core/src/com/desitum/crackTheCode/world/GameWorld.java
@@ -21,24 +21,9 @@ public class GameWorld {
 
         tiles = new ArrayList<Tile>();
         for(int tilesToDraw = 0; tilesToDraw < tileCount; tilesToDraw++) {
-            if(tilesToDraw < 4){
-                tiles.add(new Tile(1, 2f + tilesToDraw, 0, Assets.buttonTexture));
-            }
-            else if(tilesToDraw < 8){
-                tiles.add(new Tile(1, 2f + tilesToDraw -4 , 2, Assets.buttonTexture));
-            }
-            else if(tilesToDraw < 12){
-                tiles.add(new Tile(1, 2f + tilesToDraw - 8, 4, Assets.buttonTexture));
-            }
-            else if(tilesToDraw < 16){
-                tiles.add(new Tile(1, 2f + tilesToDraw - 12, 6, Assets.buttonTexture));
-            }
-            else if(tilesToDraw < 20){
-                tiles.add(new Tile(1, 2f + tilesToDraw - 16, 8, Assets.buttonTexture));
-            }
-            else{
-                tiles.add(new Tile(1, 2f + tilesToDraw - 20, 10, Assets.buttonTexture));
-            }
+            float locY = (tilesToDraw/4) * 2.4f + 0.4f;
+            float locX = (tilesToDraw%4) * 2.4f + 0.4f;
+            tiles.add(new Tile(2, locX, locY, Assets.buttonTexture));
         }
     }
 
@@ -47,6 +32,10 @@ public class GameWorld {
 
         }
 
+    }
+
+    public ArrayList<Tile> getTiles(){
+        return tiles;
     }
 
     public void reset() {
