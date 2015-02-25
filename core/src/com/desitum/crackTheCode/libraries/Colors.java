c0b0852a487f0940bf4b8012cf427f57c30a363c
Zack Moss <zmyth97@gmail.com>
2015-02-24 20:48:55 -0700

    Color Theme

--- core/src/com/desitum/crackTheCode/libraries/Colors.java
+++ core/src/com/desitum/crackTheCode/libraries/Colors.java
@@ -8,11 +8,12 @@ import java.util.ArrayList;
  * Created by Zmyth97 on 2/15/2015.
  */
 public class Colors {
-    public static final Color ACTIVE_CIRCLE = new Color(0.482f, 0.875f, 0f, 1);
-    public static final Color GAME_CIRCLE = new Color(0.106f, 0.071f, 0.671f, 1);
+    public static final Color ACTIVE_CIRCLE = new Color(1, 0, 0, 1);
+    public static final Color GAME_CIRCLE = new Color(.392f, 1, 0, 1);
     public static final Color DISABLED_CIRCLE = new Color(0.5f, 0.5f, 0.5f, 1);
 
 
+    //public static final Color ACTIVE_CIRCLE = new Color(0.482f, 0.875f, 0f, 1); //Red
     public static ArrayList<Color> Colors;
 
 
--- desktop/src/com/desitum/crackTheCode/desktop/DesktopLauncher.java
+++ desktop/src/com/desitum/crackTheCode/desktop/DesktopLauncher.java
@@ -1,12 +1,17 @@
 package com.desitum.crackTheCode.desktop;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.desitum.crackTheCode.CrackTheCode;
+import com.desitum.crackTheCode.GooglePlayServicesInterface;
 
-public class DesktopLauncher {
+public class DesktopLauncher{
+    GooglePlayServicesInterface gps;
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CrackTheCode(), config);
+
 	}
 }
