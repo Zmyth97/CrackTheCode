161fe60ae25912ac87fdacab098fd2acea9e468a
Zack Moss <zmyth97@gmail.com>
2015-02-24 21:24:17 -0700

    Final Color Themes

--- core/src/com/desitum/crackTheCode/libraries/Colors.java
+++ core/src/com/desitum/crackTheCode/libraries/Colors.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
  * Created by Zmyth97 on 2/15/2015.
  */
 public class Colors {
-    public static final Color ACTIVE_CIRCLE = new Color(1, 0, 0, 1);
-    public static final Color GAME_CIRCLE = new Color(.392f, 1, 0, 1);
+    public static final Color ACTIVE_CIRCLE = new Color(0.992f, 0.231f, 0.251f, 1);
+    public static final Color GAME_CIRCLE = new Color(0.173f, 0.635f, 0.702f, 1);
     public static final Color DISABLED_CIRCLE = new Color(0.5f, 0.5f, 0.5f, 1);
 
 
--- core/src/com/desitum/crackTheCode/screens/MainScreen.java
+++ core/src/com/desitum/crackTheCode/screens/MainScreen.java
@@ -91,7 +91,7 @@ public class MainScreen implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-       Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+       Gdx.gl.glClearColor(1f, 1f, 1f, 1);
 
 
 
